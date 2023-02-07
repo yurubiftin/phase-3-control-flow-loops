88ea3533e6095bb3fdff42274305ab5026a23e48
yurubahmed <yurubiftin2002@gmail.com>
2023-02-07 08:48:15 -0500

    done

--- looping.rb
+++ looping.rb
@@ -1,6 +1,14 @@
 def happy_new_year
   # your code here
+  counter = 10;
+  while counter > 0 
+    puts(counter);
+    counter-=1
+  end 
+  puts("Happy New Year!")
+  
 end
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -17,8 +25,21 @@ end
 
 def fizzbuzz_printer
   # your code here
+  for num in 1..100
+    puts fizzbuzz(num)
+  end
+
 end
 
 def reverse_string(str)
   # your code here
+  # Write a method `reverse_string` that takes one argument, a string, and reverses
+  # it. Don't use the built-in `.reverse` method. Instead, loop through the
+  # characters in the input string and reverse it.
+  reverse_string = ""
+  for char in str.chars
+    reverse_string = char + reverse_string
+  end
+  reverse_string
+  
 end
